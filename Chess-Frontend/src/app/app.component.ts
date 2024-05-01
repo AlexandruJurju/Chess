c6d9d44b983d8155692e5f40f7745869a056c214
Alexandru.I.Jurju <alexandru.i.jurju@gmail.com>
2024-05-01 13:28:48 +0300

    highlight valid moves

--- Chess-Frontend/src/app/app.component.ts
+++ Chess-Frontend/src/app/app.component.ts
@@ -1,20 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {NgForOf, NgIf} from "@angular/common";
-import {ApiModule, BoardDto, ChessService, PieceDto, Player} from "../services/openapi";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {ApiModule, BoardDto, ChessService, Move, PieceDto, Player, Position} from "../services/openapi";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ApiModule, NgForOf, NgIf],
+  imports: [RouterOutlet, ApiModule, NgForOf, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-
   // @ts-ignore
   boardDto: BoardDto;
+  validMoves: Move[] = [];
 
   constructor(private chessService: ChessService) {
   }
@@ -29,5 +29,17 @@ export class AppComponent implements OnInit {
   getPieceImage(piece: PieceDto): string {
     // @ts-ignore
     return `../assets/pieces/${piece.color}-${piece.type}.png`;
+  }
+
+  getValidMoves(position: Position) {
+    this.chessService.getValidMoves(position).subscribe(moves => {
+      this.validMoves = moves;
+      console.log(moves);
+    });
+  }
+
+  isMoveValid(position: Position): boolean {
+    // @ts-ignore
+    return this.validMoves.some(move => move.endPosition.row === position.row && move.endPosition.column === position.column);
   }
 }
--- Chess-Frontend/src/services/openapi/api/chess.service.ts
+++ Chess-Frontend/src/services/openapi/api/chess.service.ts
@@ -19,6 +19,7 @@ import { Observable }                                        from 'rxjs';
 
 import { BoardDto } from '../model/boardDto';
 import { Move } from '../model/move';
+import { Position } from '../model/position';
 
 import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
 import { Configuration }                                     from '../configuration';
@@ -86,6 +87,54 @@ export class ChessService {
 
         return this.httpClient.request<BoardDto>('get',`${this.basePath}/api/Chess`,
             {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * 
+     * 
+     * @param body 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public getValidMoves(body?: Position, observe?: 'body', reportProgress?: boolean): Observable<Array<Move>>;
+    public getValidMoves(body?: Position, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<Array<Move>>>;
+    public getValidMoves(body?: Position, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<Array<Move>>>;
+    public getValidMoves(body?: Position, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+
+        let headers = this.defaultHeaders;
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'text/plain',
+            'application/json',
+            'text/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json',
+            'text/json',
+            'application/_*+json'
+        ];
+        const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+        if (httpContentTypeSelected != undefined) {
+            headers = headers.set('Content-Type', httpContentTypeSelected);
+        }
+
+        return this.httpClient.request<Array<Move>>('post',`${this.basePath}/api/Chess/valid`,
+            {
+                body: body,
                 withCredentials: this.configuration.withCredentials,
                 headers: headers,
                 observe: observe,
