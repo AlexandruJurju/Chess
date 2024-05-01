634c2ab389399d09a7070410c20f79a97430b6d4
Alex <alexandru.i.jurju@gmail.com>
2024-05-01 13:42:59 +0300

    added way to select and deselect piece

--- Chess-Frontend/src/app/app.component.ts
+++ Chess-Frontend/src/app/app.component.ts
@@ -14,6 +14,7 @@ import {ApiModule, BoardDto, ChessService, Move, PieceDto, Player, Position} fro
 export class AppComponent implements OnInit {
   boardDto: BoardDto = {} as BoardDto;
   validMoves: Move[] = [];
+  selectedPiece: PieceDto | null = null;
 
   constructor(private chessService: ChessService) {
   }
@@ -26,12 +27,28 @@ export class AppComponent implements OnInit {
   }
 
   getPieceImage(piece: PieceDto): string {
-    // @ts-ignore
     return `../assets/pieces/${piece.color}-${piece.type}.png`;
   }
 
   getCurrentPlayer(): string {
     return this.boardDto.player!;
+  }
+
+  selectPiece(piece: PieceDto) {
+    if (this.selectedPiece === piece) {
+      this.selectedPiece = null; // Deselect the piece if it's clicked again
+      this.validMoves = []; // Clear the valid moves
+    } else {
+      this.selectedPiece = piece; // Select the piece
+      this.getValidMoves(piece.position!); // Get the valid moves for the selected piece
+    }
+  }
+
+
+  movePiece(position: Position) {
+    if (this.selectedPiece && this.isMoveValid(position)) {
+      console.log(`Moving piece from ${this.selectedPiece.position} to ${position}`);
+    }
   }
 
   getValidMoves(position: Position) {
