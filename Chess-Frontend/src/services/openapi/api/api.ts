a919ce5c571d16f4a1bcee9e9f09c020e0d9d110
Alex <alexandru.i.jurju@gmail.com>
2024-05-03 08:51:31 +0300

    added openapi services for backend

---
+++ Chess-Frontend/src/services/openapi/api/api.ts
@@ -0,0 +1,3 @@
+export * from './chess.service';
+import { ChessService } from './chess.service';
+export const APIS = [ChessService];
---
+++ Chess-Frontend/src/services/openapi/model/models.ts
@@ -0,0 +1,7 @@
+export * from './boardDto';
+export * from './gameDto';
+export * from './move';
+export * from './pieceDto';
+export * from './pieceType';
+export * from './player';
+export * from './position';
