3675ccbb5482cddc24e0183713bcc0a4ea83fce7
Alex <alexandru.i.jurju@gmail.com>
2024-05-03 09:03:20 +0300

    added model view controller

--- Chess-Console/Chess-Console/Program.cs
+++ Chess-Console/Chess-Console/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Controller controller = new Controller();
+        controller.Run();
     }
 }
